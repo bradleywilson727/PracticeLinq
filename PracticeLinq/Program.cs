7fcbefa95d299c78b9e346e09cfc9120421c5d66
Bradley Wilson <bradleywilson727@gmail.com>
2022-06-21 13:33:56 -0500

    completed assignment

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGamesList = new List<string>();
+            videoGamesList.Add("Legend of Zelda");
+            videoGamesList.Add("Halo");
+            videoGamesList.Add("Destiny");
+            videoGamesList.Add("Super Mario Bros.");
+            videoGamesList.Add("World of Warcraft");
+            videoGamesList.Add("Call of Duty");
+            videoGamesList.Add("Smash Brothers");
+            videoGamesList.Add("Mass Effect");
+
+            var nameByLength = videoGamesList.OrderBy(x => x.Length);
+            foreach(var game in nameByLength)
+            {
+                Console.WriteLine(game);
+            }
+
+        }
+    }
+}
